5c5a1e34ec073b6674eac642e5a4774db228959a
Amodu <amodudavid777@gmail.com>
2023-11-22 15:29:18 +0100

    added a sequence diagram

--- jobsearcher.py
+++ jobsearcher.py
@@ -8,15 +8,17 @@ def job_search(provider, location, job_title):
 
 
 class JobSearcher:
-    def __init__(self, location, job_title):
-        self.__location = location
-        self.__job_title = job_title
+    def __init__(self):
+        self.__location = None
+        self.__job_title = None
         self.__indeed_factory = IndeedFactory()
         self.__jobberman_factory = JobbermanFactory()
 
-    def searcher(self):
+    def searcher(self, location, job_title):
         """ Search for jobs across the various job mechanism in place """
+        self.__location = location
+        self.__job_title = job_title
         indeed_provider = self.__indeed_factory.create()
         jobberman_provider = self.__jobberman_factory.create()
 
-        indeed_result = Thread(target=job_search, )
+        # indeed_result = Thread(target=job_search, )
